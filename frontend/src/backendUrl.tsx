3e63dccac69acb2f54b759b26fb93114adc4a6e9
Hrx4 <dragkamal71@gmail.com>
2025-10-27 13:36:57 +0530

    url changed

--- frontend/src/backendUrl.tsx
+++ frontend/src/backendUrl.tsx
@@ -1,2 +1,2 @@
-export const backendUrl = 'http://localhost:5000';
-// export const backendUrl = 'https://upivot-assignment.vercel.app';
+// export const backendUrl = 'http://localhost:5000';
+export const backendUrl = 'https://upivot-assignment.vercel.app';
